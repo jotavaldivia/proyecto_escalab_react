5fa149681cf69d62116df69a11347cae6ffec47f
Juan Valdebenito <valdebenitovaldiviajuan@gmail.com>
2023-08-15 20:09:29 -0400

    change: update vite.config.js

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import s from "./Card.module.css";
 const Card = ({ data }) => {
   const { name, image, species, status } = data;
-  console.log(data);
+  //console.log(data);
   return (
     <div className={s.card}>
       <img src={image} alt="Rick and Morty" />
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import s from "./Search.module.css";
+import PropTypes from "prop-types";
 
 const InputSearch = styled.input`
   border: 1px solid #ccc;
   border-radius: 5px;
   padding: 5px;
-  width: 80%;
+  width: 100%;
   margin: 10px;
   font-size: 18px;
   color: #333;
@@ -25,33 +26,25 @@ const InputSearch = styled.input`
   }
 `;
 
-const ButtonSearch = styled.button`
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 5px;
-  width: 100px;
-  margin: 10px;
-  font-size: 18px;
-  color: #333;
-  outline: none;
-  transition: 0.3s ease all;
+const Search = ({ data }) => {
+  const { hanlderSearch } = data;
 
-  &:hover {
-    border-color: #000;
-  }
-  &:focus {
-    border-color: #000;
-    box-shadow: 0 0 5px 0 #000;
-  }
-`;
-
-const Search = () => {
   return (
     <div className={s.search}>
-      <ButtonSearch> BUSCAR </ButtonSearch>
-      <InputSearch placeholder="Ricky Sanchez..." />
+      <form className={s.form}>
+        <InputSearch
+          placeholder="Buscar : Rick Sanchez..."
+          onChange={(e) => {
+            hanlderSearch(e.target.value);
+          }}
+        />
+      </form>
     </div>
   );
 };
 
 export default Search;
+
+Search.propTypes = {
+  data: PropTypes.object.isRequired,
+};
